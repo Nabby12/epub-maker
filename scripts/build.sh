9fcb59e63021a2b6562a51082334ef5946db54f5
Nabby12 <Nabby12@users.noreply.github.com>
2022-12-30 03:49:07 +0900

    modified/build-shell

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,13 +4,21 @@ set -e
 
 source ./.env
 
+start_yellow="\033[0;33m"
+end_yellow="\033[0m"
+start_green="\033[0;32m"
+end_green="\033[0m"
+
 # 対象のファイルを一部表示
-echo -e "\n==========> target files..."
+echo -e "\n"
+echo -e "${start_yellow}==========> target files...${end_yellow}"
 
 ls -la ${ASSET_DIR} | head -10 | xargs -I{} echo {} && echo -e "...\n"
 
 # 必要ファイル生成
-echo -e "\n==========> setting template..."
+echo -e "\n"
+echo -e "${start_yellow}==========> setting template...${end_yellow}"
+echo "making directories and copying template files."
 ## 必要フォルダ作成
 rm -rf ${BUILD_DIR}
 mkdir -p ${META_DIR}
@@ -26,20 +34,23 @@ cp template/OEBPS/Styles/style.css ${EPUB_STYLE_DIR}/style.css
 cp template/OEBPS/Text/cover.xhtml ${EPUB_TEXT_DIR}/cover.xhtml
 cp template/OEBPS/Text/nav.xhtml ${EPUB_TEXT_DIR}/nav.xhtml
 cp ${ASSET_DIR}/cover.jpg ${EPUB_IMAGE_DIR}/cover.jpg
-echo "==========> setting done."
+echo -e "${start_yellow}==========> setting done.${end_yellow}"
 
 # 画像修正（リサイズ・トリミング・名前変更）
-echo -e "\n==========> processing images..."
+echo -e "\n"
+echo -e "${start_yellow}==========> processing images...${end_yellow}"
 node src/trim_and_rename.js
-echo "==========> processing done."
+echo -e "${start_yellow}==========> processing done.${end_yellow}"
 
 # ページ生成
-echo -e "\n==========> generating pages..."
+echo -e "\n"
+echo -e "${start_yellow}==========> generating pages...${end_yellow}"
 ./scripts/generate.sh
-echo "==========> generating done."
+echo -e "${start_yellow}==========> generating done.${end_yellow}"
 
 # epubファイル生成
-echo -e "\n==========> exporting epub file..."
+echo -e "\n"
+echo -e "${start_yellow}==========> exporting epub file...${end_yellow}"
 ## epubファイル出力フォルダを生成
 rm -rf ${OUTPUT_DIR}
 mkdir -p ${OUTPUT_DIR}
@@ -49,6 +60,7 @@ current_dir="$(pwd)"
 cd ${BUILD_DIR}
 zip -X0 "${current_dir}/${OUTPUT_DIR}/${BOOK_TITLE}.epub" mimetype
 zip -r9 "${current_dir}/${OUTPUT_DIR}/${BOOK_TITLE}.epub" * -x mimetype
-echo "==========> exporting done."
+echo -e "${start_yellow}==========> exporting done.${end_yellow}"
 
-echo -e "\nbuild epub succeeded!! :)"
+echo -e "\n"
+echo -e "${start_green}build epub succeeded!! :)${end_green}"
